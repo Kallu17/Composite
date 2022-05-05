92a1e75fd8e94a28bc89cd66d2044c6ffa96f2d9
Kallu <56933682+Kallu17@users.noreply.github.com>
2022-05-05 16:07:20 -0300

    Create GrandParent.java

---
+++ GrandParent.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrandParent implements Human
+{
+    private String name;
+    private String lastName;
+
+    @Override
+    public String getName() 
+    {
+        return name;
+    }
+
+    @Override
+    public String getLastName() 
+    {
+        return lastName;
+    }
+
+    public GrandParent(String name, String lastName)
+    {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public void print() 
+    {
+        System.out.println("==============================");
+        System.out.println("Nome : " + getName());
+        System.out.println("Sobrenome : " + getLastName());
+        System.out.println("Filho(s) :");
+        for (Human child: childs) {
+            System.out.println("  - Nome : " + child.getName());
+        }
+        System.out.println("==============================");
+    }
+
+    private List<Human> childs = new ArrayList<>();
+    @Override
+    public void addChild(Human child) 
+    {
+        childs.add(child);
+    }
+
+    @Override
+    public void addParent(Human parent)
+    {
+
+    }
+}
